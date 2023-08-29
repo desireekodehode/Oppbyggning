37b030774493b3ab4de6bcb3d48c30255859fd50
desireekodehode <desireekodehode1@gmail.com>
2023-08-29 14:43:54 +0200

    dag1

---
+++ script.js
@@ -0,0 +1,32 @@
+console.log("hei på deg")
+
+let firstName= "John"
+let lastName= "Doe"
+
+console.log(firstName)
+
+console.log(lastName)
+let conFullName = `My name is ${firstName} `
+console.log
+firstName= "Bob"
+
+console.log(firstName)
+
+let fullName= "My name is " + firstName + lastName 
+
+console.log(fullName)
+
+console.log(1+2) 
+
+let randomNumber = Math.random() * 5 + 1
+let roundedNumber = Math.round(randomNumber) 
+
+console.log (roundedNumber)
+
+
+console.log 
+let suggestion = "hei" 
+console.log
+
+
+
